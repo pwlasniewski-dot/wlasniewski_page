d23348acc704e9721a5205ba75b3da480255f590
pwlasniewski-dot <pwlasniewski@gmail.com>
2025-12-21 22:41:23 +0100

    Clean up debug logging from auth middleware

--- create_admin.js
+++ create_admin.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 async function main() {
     const email = 'pwlasniewski@gmail.com';
-    const password = 'Fotograf2025!';
+    const password = 'admin123';
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await prisma.adminUser.upsert({
